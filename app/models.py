03d0da3d4d2213f03ffadb76969b1b478f9d9ed9
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-02-19 12:05:19 -0500

    Databases are now work. Attempting to get user log in memorized

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model): # working table for user registration
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -21,15 +21,17 @@ class User(UserMixin, db.Model):
     def check_password_hash(self, password):
         return check_password_hash(self.password_hash, password)
     
-    @login.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
 
-class Hero(db.Model):
+
+class Hero(db.Model): # table now works, but the foreignkey syntax is throwing errors every time I try to use it.
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     description = db.Column(db.String(255))
     comics_appeared = db.Column(db.Integer, index=True)
     super_power = db.Column(db.String(255), index=True)
     date_created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    owner = db.relationship(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -12,21 +12,23 @@ def front_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('front_page'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-    return render_template('login.html', title='Login')
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('front_page'))
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('front_page'))
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST']) #user registration now working, but I had to disable the class for the Hero table
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('front_page'))
@@ -37,5 +39,5 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Welcome to Build Your Own Avengers')
-        return redirect(url_for('login'))
+        return redirect('login')
     return render_template('register.html', title='Register', form=form)
